6d5b659af11eac049d8adea932c61bceef10ee2f
GregoryLuke <geanevaitbecke@gmail.com>
2020-12-14 04:00:16 +0800

    refactor: simplify cap
    Signed-off-by: GregoryLuke <geanevaitbecke@gmail.com>

--- core/cap.py
+++ core/cap.py
@@ -3,19 +3,21 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 from functools import wraps
 from pathlib import Path
+from xml.etree.ElementTree import Element, SubElement, ElementTree
 
 from core.argument import load_argument, check_number_parser
 from core.comm import (
+    mkdir,
+    move,
+    create_folder,
     check_data_state,
     extra_tag,
-    create_successfull_folder,
-    write_nfo,
-    rename_move_file,
+    replace_date,
+    check_name_length,
 )
 from crawler.crawlerComm import CrawlerBase, WebsitePriority
 from crawler.registerService import auto_register_service
 from utils.logger import setup_logger
-from utils.path import PathHandler
 
 thePoolsize = 3
 
@@ -32,126 +34,149 @@ def thread_pool(f):
     return wrap
 
 
-class CapBase:
+"""
+capture single file  according file path and number
+根据文件地址和对应编号刮削单个文件
+获取元数据 -> 创建文件夹 -> 重命名和移动文件 -> 下载和处理图片 -> 创建 nfo
+"""
+
+
+@thread_pool
+def get_metadata(services, file, number, cfg):
     """
-    capture single file  according file path and number
-    根据文件地址和对应编号刮削单个文件
-    获取元数据 -> 创建文件夹 -> 重命名和移动文件 -> 下载和处理图片 -> 创建 nfo
+    get metadata from website according to number
+    根据编号从网站爬取元数据
+    依次从各个网站中爬取，根据特定编号排列优先级
+    Returns: metadata
     """
+    # priority init， get sorted website
+    priority = WebsitePriority(cfg.priority.website)
+    # priority.sort_website(self.number)
+    while not priority.empty():
+        # noinspection PyBroadException
+        try:
+            data = services.get(priority.pop(), number, cfg)
+            if check_data_state(data):
+                return extra_tag(file, data)
+            continue
+        except Exception as exc:
+            logger.error(f"No data obtained: {exc}")
 
-    def __init__(
-        self,
-        file: Path,
-        number: str,
-        search_folder: Path,
-        failed_folder: Path,
-        cfg,
-        services,
-    ):
-        """
-        Args:
-            file: 原文件地址
-            number: 番号
-            search_folder: 搜索地址，单文件搜索即为文件地址，否则为文件夹地址
-            failed_folder: 创建的失败文件夹地址
-            cfg: 配置
-        """
-        self.file = file
-        self.number = number
-        self.search_folder = search_folder
-        self.failed_folder = failed_folder
-        self.cfg = cfg
-        self.services = services
 
-    @classmethod
-    def parameter(cls, file, number, search_folder, failed_folder, cfg, services):
-        """
-        类方法，外部访问，传入参数，并创建一个实例
-        """
-        return cls(file, number, search_folder, failed_folder, cfg, services)
+@thread_pool
+def folder_utils(cfg, search_path, data):
+    """
+    use metadate replace location_rule, create folder
+    使用爬取的元数据替换路径规则，再创建文件。
+    根据 / 划分层级，检查每层文件夹的名称长度
+    Args:
+        cfg:
+        search_path:
+        data: metadata
+    """
+    # If the number of actors is more than 5, take the first two
+    # 如果演员数量大于5 ，则取前两个
+    if len(data.actor.split(",")) >= 5:
+        data.actor = ",".join(data.actor.split(",")[:2]) + "etc."
+    location_rule = replace_date(data, cfg.name_rule.location_rule)
+    # mkdir folder by level
+    new_folder = None
+    for name in location_rule.split("/"):
+        name = check_name_length(name, cfg.name_rule.max_title_len)
+        output_folder = folder_utils(
+            search_path, cfg.name_rule.success_output_folder, cfg
+        )
+        new_folder = mkdir(output_folder.joinpath(name))
+    return new_folder
 
-    @thread_pool
-    def get_metadata(self):
-        """
-        get metadata from website according to number
-        根据编号从网站爬取元数据
-        依次从各个网站中爬取，根据特定编号排列优先级
-        Returns: metadata
-        """
-        # priority init， get sorted website
-        priority = WebsitePriority(self.cfg.priority.website)
-        # priority.sort_website(self.number)
-        while not priority.empty():
-            # noinspection PyBroadException
-            try:
-                data = self.services.get(priority.pop(), self.number, self.cfg)
-                if check_data_state(data):
-                    return extra_tag(self.file, data)
-                continue
-            except Exception as exc:
-                logger.error(f"No data obtained: {exc}")
 
-    @thread_pool
-    def folder_utils(self, data):
-        """
-        use metadate replace location_rule, create folder
-        使用爬取的元数据替换路径规则，再创建文件。
-        根据 / 划分层级，检查每层文件夹的名称长度
-        Returns: 已创建的文件夹地址
+@thread_pool
+def file_utils(file, created_folder, data, cfg):
+    """
+    重命名和移动文件
+    Returns: 重命名和移动之后的文件地址
 
-        """
-        return create_successfull_folder(self.search_folder, data, self.cfg)
+    """
+    # 替换数据，检查长度，移动和重命名文件，
+    # check length of name
+    naming_rule = check_name_length(
+        cfg.name_rule.naming_rule, cfg.name_rule.max_title_len
+    )
+    file_name = replace_date(data, naming_rule)
 
-    @thread_pool
-    def file_utils(self, created_folder, data):
-        """
-        重命名和移动文件
-        Returns: 重命名和移动之后的文件地址
+    for mark in data.extra.values():
+        file_name += "-" + mark
 
-        """
-        return rename_move_file(self.file, created_folder, data, self.cfg)
+    file_name += file.suffix
+    new_file_path = created_folder.joinpath(file_name)
+    move(file, new_file_path)
+    return new_file_path
 
-    @thread_pool
-    def img_utils(self, created_folder: Path, data):
-        """
-        download and process pic
-        处理和下载图片
-        Args:
-            data:
-            created_folder: 已创建的文件夹地址
-        """
-        request = CrawlerBase(self.cfg)
-        # 伪代码
-        img_url = {"poster": data.poster,
-                   "thumb": data.thumb, "fanart": data.fanart}
-        request.download_all(img_url, created_folder)
-        # //TODO 裁剪，水印
 
-    @thread_pool
-    def create_nfo(self, new_file_path: Path, data):
-        """
-        创建 nfo 文件
-        Args:
-            data:
-            new_file_path: 用于 nfo 名称和 nfo文件位置
-        """
+@thread_pool
+def img_utils(created_folder: Path, data, cfg):
+    """
+    download and process pic
+    处理和下载图片
+    Args:
+        cfg:
+        data:
+        created_folder: 已创建的文件夹地址
+    """
+    request = CrawlerBase(cfg)
+    # 伪代码
+    img_url = {"poster": data.poster,
+               "thumb": data.thumb, "fanart": data.fanart}
+    request.download_all(img_url, created_folder)
+    # //TODO 裁剪，水印
 
-        write_nfo(new_file_path, data, self.cfg)
 
-    async def start(self):
-        start_time = time.perf_counter()
-        logger.info(f"searching: {self.number}")
-        data = await self.get_metadata()
-        if not data and not self.cfg.debug.enable:
-            PathHandler.move(self.file, self.failed_folder, flag="failed")
-        print(data)
-        # created_folder = await self.folder_utils(data)
-        # new_file_path = await self.file_utils(created_folder, data)
-        # await self.img_utils(created_folder, data)
-        # await self.create_nfo(new_file_path, data)
-        end_time = time.perf_counter() - start_time
-        logger.debug(
-            f"searching: {self.number} (took {end_time:0.2f} seconds).")
+@thread_pool
+def create_nfo(file_path: Path, data, cfg):
+    """
+    //TODO fields写完
+    Args:
+        cfg:
+        file_path:
+        data:
+    """
+    nfo_root = Element("movie")
+    folder = Path(file_path).parent
+    filename = folder.joinpath(Path(file_path).with_suffix(".nfo"))
+    nfo_fields = dict
+    if cfg.common.mode == "":
+        nfo_fields = {
+            "title": data.title,
+            "studio": data.studio,
+            "year": data.year,
+            "tag": data.tag,
+        }
+    for field_name, values in nfo_fields.items():
+        if not values:
+            continue
+        if not isinstance(values, list):
+            values = [values]
+        for value in values:
+            SubElement(nfo_root, field_name).text = f"{value}"
+
+    ElementTree(nfo_root).write(
+        filename, encoding="utf-8", xml_declaration=True)
+
+
+async def start(services, file, number, failed_folder, cfg):
+    start_time = time.perf_counter()
+    logger.info(f"searching: {number}")
+    data = await get_metadata(cfg, services, file, number)
+    if not data and not cfg.debug.enable:
+        move(file, failed_folder, flag="failed")
+    print(data)
+    # created_folder = await self.folder_utils(data)
+    # new_file_path = await self.file_utils(created_folder, data)
+    # await self.img_utils(created_folder, data)
+    # await self.create_nfo(new_file_path, data)
+    end_time = time.perf_counter() - start_time
+    logger.debug(
+        f"searching: {number} (took {end_time:0.2f} seconds).")
 
 
 async def capture():
@@ -161,7 +186,7 @@ async def capture():
     # deubg 不创建文件夹
     if not cfg.debug.enable:
         filed_folder = cfg.common.failed_output_folder
-        failed = PathHandler.create_folder(folder, filed_folder)
+        failed = create_folder(folder, filed_folder)
     else:
         failed = None
 
@@ -176,8 +201,5 @@ async def capture():
     # 传入参数，创建任务循环
     tasks = []
     for file, number in target.items():
-        tasks.append(
-            CapBase.parameter(file, number, folder, failed,
-                              cfg, services).start()
-        )
+        tasks.append(start(services, file, number, failed, cfg))
     await asyncio.gather(*tasks)
--- core/comm.py
+++ core/comm.py
@@ -2,13 +2,67 @@
 对应 argument， 这里放每一个视频都会运行的函数
 """
 import re
+import shutil
 from pathlib import Path
-from xml.etree.ElementTree import Element, SubElement, ElementTree
 
 from utils.logger import setup_logger
-from utils.path import PathHandler
 
 logger = setup_logger()
+
+
+def move(src: Path, dest: Path, flag: str = None):
+    """
+    移动文件
+    Args:
+        src:
+        dest:
+        flag: faild or successfull
+    """
+    assert src.exists()
+    try:
+        shutil.move(src, dest)
+        logger.info(f"move {src.name} to {flag} folder")
+    except Exception as exc:
+        logger.error(f"fail to move file: {str(exc)}")
+
+
+def mkdir(src: Path):
+    """
+    创建文件夹，并返回已创建的文件夹
+    Args:
+        src:
+
+    Returns:
+
+    """
+    assert src.exists()
+    try:
+        src.mkdir()
+        logger.info(f"succeed create folder: {src.as_posix()}")
+        return src
+    except Exception as exc:
+        logger.error(f"fail to create folder: {str(exc)}")
+
+
+def symlink():
+    raise NotImplementedError()
+
+
+def create_folder(search_path: Path, needed_create: str):
+    """
+    根据相对路径和绝对路径创建文件夹
+    Args:
+        search_path:
+        needed_create:
+
+    Returns:
+
+    """
+    folder = Path(needed_create).resolve()
+    if not folder.is_absolute():
+        created = search_path.parent.joinpath(needed_create)
+        return mkdir(created)
+    return mkdir(folder)
 
 
 def check_data_state(data) -> bool:
@@ -75,78 +129,3 @@ def check_name_length(name, max_title_len) -> str:
             return name[:max_title_len]
         check_name_length(choice, max_title_len)
     return name
-
-
-def create_successfull_folder(search_path, data, cfg):
-    """
-    use metadate replace location_rule, create folder
-    使用爬取的元数据替换路径规则，再创建文件。
-    根据 / 划分层级，检查每层文件夹的名称长度
-    Args:
-        cfg:
-        search_path:
-        data: metadata
-    """
-    # If the number of actors is more than 5, take the first two
-    # 如果演员数量大于5 ，则取前两个
-
-    if len(data.actor.split(",")) >= 5:
-        data.actor = ",".join(data.actor.split(",")[:2]) + "etc."
-    location_rule = replace_date(data, cfg.name_rule.location_rule)
-    # mkdir folder by level
-    new_folder = None
-    for name in location_rule.split("/"):
-        name = check_name_length(name, cfg.name_rule.max_title_len)
-        output_folder = create_successfull_folder(
-            search_path, cfg.name_rule.success_output_folder, cfg
-        )
-        new_folder = PathHandler.mkdir(output_folder.joinpath(name))
-    return new_folder
-
-
-def rename_move_file(old_file_path, new_folder_path, data, cfg):
-    # 替换数据，检查长度，移动和重命名文件，
-    # check length of name
-    naming_rule = check_name_length(
-        cfg.name_rule.naming_rule, cfg.name_rule.max_title_len
-    )
-    file_name = replace_date(data, naming_rule)
-
-    for mark in data.extra.values():
-        file_name += "-" + mark
-
-    file_name += old_file_path.suffix
-    new_file_path = new_folder_path.joinpath(file_name)
-    PathHandler.move(old_file_path, new_file_path)
-    return new_file_path
-
-
-def write_nfo(file_path, data, cfg):
-    """
-    //TODO fields写完
-    Args:
-        cfg:
-        file_path:
-        data:
-    """
-    nfo_root = Element("movie")
-    folder = Path(file_path).parent
-    filename = folder.joinpath(Path(file_path).with_suffix(".nfo"))
-    nfo_fields = dict
-    if cfg.common.mode == "":
-        nfo_fields = {
-            "title": data.title,
-            "studio": data.studio,
-            "year": data.year,
-            "tag": data.tag,
-        }
-    for field_name, values in nfo_fields.items():
-        if not values:
-            continue
-        if not isinstance(values, list):
-            values = [values]
-        for value in values:
-            SubElement(nfo_root, field_name).text = f"{value}"
-
-    ElementTree(nfo_root).write(
-        filename, encoding="utf-8", xml_declaration=True)
